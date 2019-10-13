0b7a408314bca60d2b0514cd4b85e2f5c569905a
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-10-13 09:13:01 +0200

    Add calculate class

---
+++ Demo1/src/calc/Calculate.java
@@ -0,0 +1,19 @@
+package calc;
+
+public class Calculate {
+	private int add(int a, int b) {
+		return a + b;
+	}
+	
+	private int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	private int subtract(int a, int b) {
+		return a - b;
+	}
+
+	private int divide(int a, int b) {
+		return a / b;
+	}
+}
