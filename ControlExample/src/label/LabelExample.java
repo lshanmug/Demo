78444d0cf21ae422e6992e6675c3cf10260e53b3
User1 <lshanmug@in.ibm.com>
2019-10-14 13:44:48 +0200

    added label example

---
+++ ControlExample/src/label/LabelExample.java
@@ -0,0 +1,34 @@
+package label;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class LabelExample {
+public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.setLayout(new GridLayout(1, false));
+
+	Group group = new Group(shell, SWT.NONE);
+	group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+	group.setLayout(new GridLayout(1, false));
+	group.setText("label group");
+	
+	Label label = new Label(group, SWT.NONE);
+	label.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+	label.setText("text label");
+
+	shell.pack();
+	shell.open();
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch())
+			display.sleep();
+	}
+	display.dispose();
+}
+}
