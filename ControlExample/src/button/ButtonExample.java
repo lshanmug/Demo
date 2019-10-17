804683f0cdb2ad34e7bfd34dbcf7c85c33e708b3
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-10-17 18:26:59 +0200

    created toggle button

--- ControlExample/src/button/ButtonExample.java
+++ ControlExample/src/button/ButtonExample.java
@@ -68,6 +68,10 @@ public class ButtonExample {
 		button4.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		button4.setText("Toggle button444");
 		
+		button4 = new Button(group, SWT.TOGGLE);
+		button4.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		button4.setText("Toggle button45");
+		
 		shell.pack();
 		shell.open();
 		while (!shell.isDisposed()) {
