48b75d1da347cfd55d8a5d9f86260d2416851b7d
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-10-17 22:31:48 +0200

    Added implementation

--- ControlExample/src/button/ButtonExample2.java
+++ ControlExample/src/button/ButtonExample2.java
@@ -1,7 +1,21 @@
 package button;
 
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
 public class ButtonExample2 {
 public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.setLayout(new GridLayout(1, false));
 
+	shell.pack();
+	shell.open();
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch())
+			display.sleep();
+	}
+	display.dispose();
 }
 }
