0f7d60699ee11df7aa97643090c2812618cfcb79
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-10-18 13:31:05 +0200

    Refactored button examples

--- ControlExample/src/button/ButtonExample1.java
+++ ControlExample/src/button/ButtonExample1.java
@@ -11,38 +11,11 @@ import org.eclipse.swt.widgets.Shell;
 
 public class ButtonExample1 {
 	
-	static void createButtons(Composite parent) {
+	public static void createButtons(Composite parent) {
 		createButton(parent, SWT.PUSH, "Push button");
 	}
 	
-	public static void main(String[] args) {
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setLayout(new GridLayout(1, false));
-		shell.setText("Buttons");
-		
-		Group group = createGroup(shell);
-		createButtons(group);
-		
-		shell.pack();
-		shell.open();
-		
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		display.dispose();
-	}
-
-	private static Group createGroup(Shell shell) {
-		Group group = new Group(shell, SWT.NONE);
-		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		group.setLayout(new GridLayout(1, false));
-		group.setText("Button group");
-		return group;
-	}
-
-	private static Button createButton(Composite parent, int style, String text) {
+	public static Button createButton(Composite parent, int style, String text) {
 		Button button = new Button(parent, style);
 		button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		button.setText(text);
--- ControlExample/src/button/ButtonExample2.java
+++ ControlExample/src/button/ButtonExample2.java
@@ -1,7 +1,10 @@
 package button;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 
 public class ButtonExample2 {
@@ -10,6 +13,13 @@ public static void main(String[] args) {
 	Shell shell = new Shell(display);
 	shell.setLayout(new GridLayout(1, false));
 
+	Group group = new Group(shell, SWT.NONE);
+	group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+	group.setLayout(new GridLayout(1, false));
+	group.setText("Button group");
+	
+	ButtonExample1.createButtons(group);
+	
 	shell.pack();
 	shell.open();
 	while (!shell.isDisposed()) {
